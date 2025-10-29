1b2e7e5014017ad0db44416c355795c3b8f6fd28
Kayleigh Soane <kayleigh.soane@students.plymouth.ac.uk>
2025-10-29 08:52:49 +0000

    Change location

--- COMP1001 Assignment 1/q1.asm/q1.asm/q1.asm
+++ COMP1001 Assignment 1/q1.asm/q1.asm/q1.asm
@@ -7,19 +7,19 @@ ExitProcess PROTO, dwExitCode: DWORD  ; Exit process prototype
 
 .data ; data segment
 
-qarray DWORD 10, 20, 30, 40
+array DWORD 10, 20, 30, 40
 
 
 .code ; code segment
 
 main PROC ; main procedure
 
-mov eax, [qarray + TYPE qarray*3]
 mov ecx, 3
+mov eax, [array + TYPE array*3]
 mul ecx
 mov ecx, 6
 div ecx
-mov [qarray + TYPE qarray*3], eax
+mov [array + TYPE array*3], eax
 
 
 INVOKE ExitProcess, 0 ; call exit function
